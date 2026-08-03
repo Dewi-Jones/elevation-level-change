0f23f495a7cbb06e2600b0a1461762337d711b0d
Dewi-Jones <53094799+Dewi-Jones@users.noreply.github.com>
2026-08-03 21:30:57 +0200

    Add files via upload
    - Added Configuration Options for Modes and Collections per Scene
    - Added function to more easily determine weather an elevation is in the range of a given level
    - Moved Logic from `getDestinationLevels` into `changeLevel`
    - Added Logic `isDestinationLevel` that determines weather a given level is a valid destination
    - Movement Options are now preserved

--- scripts/elevationLevelChange.mjs
+++ scripts/elevationLevelChange.mjs
@@ -1,6 +1,8 @@
+const id = "elevation-level-change";
+
 Hooks.on("init", () => {
 	
-	game.settings.register("elevation-level-change", "playerMovement", {
+	game.settings.register(id, "playerMovement", {
 		name: "ELEVATION_LEVEL_CHANGE.SETTINGS.playerMovement.name",
 		hint: "ELEVATION_LEVEL_CHANGE.SETTINGS.playerMovement.hint",
 		scope: "world",
@@ -10,7 +12,7 @@ Hooks.on("init", () => {
 		requiresReload: false
 	});
 	
-	game.settings.register("elevation-level-change", "skipDialog", {
+	game.settings.register(id, "skipDialog", {
 		name: "ELEVATION_LEVEL_CHANGE.SETTINGS.skipDialog.name",
 		hint: "ELEVATION_LEVEL_CHANGE.SETTINGS.skipDialog.hint",
 		scope: "world",
@@ -20,13 +22,29 @@ Hooks.on("init", () => {
 		requiresReload: false
 	});
 	
-	game.settings.register("elevation-level-change", "viewLevel", {
+	game.settings.register(id, "viewLevel", {
 		name: "ELEVATION_LEVEL_CHANGE.SETTINGS.viewLevel.name",
 		hint: "ELEVATION_LEVEL_CHANGE.SETTINGS.viewLevel.hint",
 		scope: "world",
 		config: true,
 		type: Boolean,
 		default: true,
+		requiresReload: false
+	});
+	
+	game.settings.register(id, "defaultMode", {
+		name: "ELEVATION_LEVEL_CHANGE.SETTINGS.defaultMode.name",
+		hint: "ELEVATION_LEVEL_CHANGE.SETTINGS.defaultMode.hint",
+		scope: "world",
+		config: true,
+		type: String,
+		choices: {
+			"excluded": "ELEVATION_LEVEL_CHANGE.CONFIG.mode.excluded",
+			"destination": "ELEVATION_LEVEL_CHANGE.CONFIG.mode.destination",
+			"origin": "ELEVATION_LEVEL_CHANGE.CONFIG.mode.origin",
+			"included": "ELEVATION_LEVEL_CHANGE.CONFIG.mode.included"
+		},
+		default: "included",
 		requiresReload: false
 	});
 });
@@ -36,22 +54,21 @@ Hooks.on("init", () => {
 Hooks.on("preMoveToken", (token, movement, operation) => {
 	
 	// Respect Player Movement Setting
-	if ( !game.user.isGM && !game.settings.get("elevation-level-change", "playerMovement") ) return
+	if ( !game.user.isGM && !game.settings.get(id, "playerMovement") ) return
 	
 	// Do nothing if Movement was done via API or pasting
 	// Prevents recursive behaviour and undesired triggers like Teleports or Change Level Regions
 	if ( movement.method === "api" || movement.method === "paste" ) return
 	
 	// If the token didn't move from inside to outside the current levels range, do nothing
-	// If movement is chained, and moves back into the current level's range, do nothing
-	const pendingElevation = movement.pending.waypoints.at(-1)?.elevation;
+	// If movement is chained, and ends within the current level's range, do nothing
 	const originLevel = token.parent.levels.get(token._source.level);
-	if ( movement.origin.elevation < originLevel.elevation.bottom || movement.origin.elevation >= originLevel.elevation.top ) return
-	if ( movement.destination.elevation >= originLevel.elevation.bottom && movement.destination.elevation < originLevel.elevation.top ) return
-	if ( pendingElevation >= originLevel.elevation.bottom && pendingElevation < originLevel.elevation.top ) return
+	if ( !elevationInLevel(movement.origin.elevation, originLevel) ) return
+	if ( elevationInLevel(movement.destination.elevation, originLevel) ) return
+	if ( elevationInLevel(movement.pending.waypoints.at(-1)?.elevation, originLevel) ) return
 	
 	// If there is no viable level, do nothing
-	const destinationLevels = getDestinationLevels( movement.destination.elevation, token );
+	const destinationLevels = token.parent.levels.filter(level => isDestinationLevel( level, originLevel, movement.destination.elevation ));
 	if ( destinationLevels.length === 0 ) return
 	
 	const currentAction = movement.passed.waypoints.at(-1).action;
@@ -60,6 +77,47 @@ Hooks.on("preMoveToken", (token, movement, operation) => {
 	
 	// Cancel triggering Movement
 	return false
+});
+
+// Configuration for Level Collections and Modes
+// Based on Trills GALC (https://github.com/lucky-trill/galc)
+Hooks.on("renderLevelConfig", (app, elements, context, option) => {
+	
+	const newFieldset = document.createElement("fieldset");
+	newFieldset.className = id;
+	
+	const legend = document.createElement("legend");
+	legend.innerText = "Elevation Level Change";
+	newFieldset.append(legend);
+	
+	const level = app.document;
+	const modes = [
+		{ value: "default", label: `${_loc("ELEVATION_LEVEL_CHANGE.CONFIG.mode.default")} (${_loc(`ELEVATION_LEVEL_CHANGE.CONFIG.mode.${game.settings.get(id, "defaultMode")}`)})` }, //0
+		{ value: "excluded", label: "ELEVATION_LEVEL_CHANGE.CONFIG.mode.excluded" }, //1
+		{ value: "destination", label: "ELEVATION_LEVEL_CHANGE.CONFIG.mode.destination" }, //2
+		{ value: "origin", label: "ELEVATION_LEVEL_CHANGE.CONFIG.mode.origin" }, //3
+		{ value: "included", label: "ELEVATION_LEVEL_CHANGE.CONFIG.mode.included" } //4
+	];
+	
+	const collections = new foundry.data.fields.StringField({ 
+		initial: level.getFlag(id, "collections") ?? "",
+		label: "ELEVATION_LEVEL_CHANGE.CONFIG.collections.label",
+		hint: "ELEVATION_LEVEL_CHANGE.CONFIG.collections.hint",
+		placeholder: `${_loc("ELEVATION_LEVEL_CHANGE.CONFIG.collections.placeholder")}`
+	}, {name: `flags.${id}.collections`});
+	newFieldset.append(collections.toFormGroup({localize: true}, {localize: true}));
+	
+	const mode = new foundry.data.fields.StringField({ 
+		initial: level.getFlag(id, "mode") ?? "default",
+		choices: modes,
+		required: true,
+		label: "ELEVATION_LEVEL_CHANGE.CONFIG.mode.label",
+		hint: "ELEVATION_LEVEL_CHANGE.CONFIG.mode.hint"
+	}, {name: `flags.${id}.mode`});
+	newFieldset.append(mode.toFormGroup({localize: true}, {localize: true}));
+	
+	const body = elements.querySelector('.standard-form[data-application-part="body"]')
+	body.append(newFieldset);
 });
 
 async function changeLevel( token, movement, destinationLevels, originLevel, currentAction ) {
@@ -72,9 +130,12 @@ async function changeLevel( token, movement, destinationLevels, originLevel, cur
 	const waypoints = movement.pending.waypoints.filter( w => !w.intermediate );
 	waypoints.unshift(movement.passed.waypoints.at(-1));
 	const newWaypoints = waypoints.map( w => ({ ...w, level: destinationLevel.id }));
-	await token.move(newWaypoints);
+	// TODO: Decide what happens if moving through more than one Level.
+	// Currently, only the first level is available to swap to, since the new movement doesn't trigger a level change.
+	// Only one Level Change should be triggered. More would be a bad UX.
+	await token.move(newWaypoints, { constrainOptions: movement.constrainOptions, autoRotate: movement.autoRotate, showRuler: movement.showRuler });
 	
-	if ( !game.settings.get("elevation-level-change", "viewLevel") ) return
+	if ( !game.settings.get(id, "viewLevel") ) return
 	
 	// The view isn't automatically changed for GM users or Players with another owned Token on the viewed level
 	const controlledTokens = canvas.tokens.controlled.map(t => t.id);
@@ -90,10 +151,11 @@ async function changeLevel( token, movement, destinationLevels, originLevel, cur
 	controlledTokens.forEach( token => canvas.tokens.get(token)?.control({ releaseOthers: false }))
 }
 
+// Based on Cores Change Level Behavior Dialog
 async function confirmDialog( levels, token ) {
 	
 	// Skip Confirmation Dialog if possible
-	if ( game.settings.get("elevation-level-change", "skipDialog") && levels.length === 1 ) return { level: levels[0].id }
+	if ( game.settings.get(id, "skipDialog") && levels.length === 1 ) return { level: levels[0].id }
 	
 	const questionElement = window.document.createElement("p");
     questionElement.textContent = levels.length !== 1 ? _loc("BEHAVIOR.TYPES.changeLevel.ConfirmSelect", {token: token.name})
@@ -126,7 +188,33 @@ async function confirmDialog( levels, token ) {
     });
 }
 
-function getDestinationLevels( elevation, token ) {
-	const destinationLevels = token.parent.levels.filter((l) => l.id !== token.level && elevation >= l.elevation.bottom && elevation < l.elevation.top);
-	return destinationLevels
+function isDestinationLevel( level, originLevel, elevation ) {
+	
+	// If the level is the origin level, it is not a valid option to swap to.
+	if ( level.id === originLevel.id ) return false
+	if ( !elevationInLevel(elevation, level) ) return false
+	
+	// Respect Modes and Collections
+	const modes = { "0": game.settings.get(id, "defaultMode"), "1": "excluded", "2": "destination", "3": "origin", "4": "included" };
+	const viableOriginModes = ["origin", "included"];
+	const viableDestinationModes = ["destination", "included"];
+	
+	const originMode = originLevel.getFlag(id, "mode") in modes ? modes[originLevel.getFlag(id, "mode")] : originLevel.getFlag(id, "mode") ?? game.settings.get(id, "defaultMode");
+	const originCollections = originLevel.getFlag(id, "collections")?.split(";") ?? [ "" ];
+	
+	const destinationMode = level.getFlag(id, "mode") in modes ? modes[level.getFlag(id, "mode")] : level.getFlag(id, "mode") ?? game.settings.get(id, "defaultMode");
+	const destinationCollections = level.getFlag(id, "collections")?.split(";") ?? [ "" ];
+	
+	if ( !viableOriginModes.includes(originMode) ) return false
+	if ( !viableDestinationModes.includes(destinationMode) ) return false
+	if ( !originCollections ) return true
+	if ( !destinationCollections ) return true
+	if ( !originCollections.some(e => destinationCollections.includes(e)) ) return false
+	
+	return true
+}
+
+function elevationInLevel( elevation, level) {
+	if ( elevation >= level.elevation.bottom && elevation < level.elevation.top ) return true
+	return false
 }
